929b81b1823a9d1a27a0a10b45b3c516fa56aeff
Yang Liu <liuyang12@focusmedia.cn>
2019-06-13 15:04:16 +0800

    update

--- utils.py
+++ utils.py
@@ -79,11 +79,8 @@ def accuracy(scores, targets, k=1):
 def parse_args():
     parser = argparse.ArgumentParser(description='Listen Attend and Spell')
     # general
-    parser.add_argument('--data-path', type=str, default='data/output/libri_fbank80_char30', help='source data path')
-    parser.add_argument('--batch-size', type=int, default=24, help='batch size in each context')
-    parser.add_argument('--n-jobs', type=int, default=8, help='subprocess used for torch Dataloader')
-
-
+    parser.add_argument('--input-dim', type=int, default=40, help='input dimension')
+    parser.add_argument('--encoder-hidden-size', type=int, default=512, help='encoder hidden size')
     parser.add_argument('--decoder-hidden-size', type=int, default=1024, help='decoder hidden size')
     parser.add_argument('--num-layers', type=int, default=4, help='number of encoder layers')
     parser.add_argument('--embedding-dim', type=int, default=512, help='embedding dimension')
@@ -94,6 +91,7 @@ def parse_args():
     parser.add_argument('--weight-decay', type=float, default=0.0005, help='weight decay')
     parser.add_argument('--mom', type=float, default=0.9, help='momentum')
     parser.add_argument('--emb-size', type=int, default=512, help='embedding length')
+    parser.add_argument('--batch-size', type=int, default=32, help='batch size in each context')
     parser.add_argument('--checkpoint', type=str, default=None, help='checkpoint')
     args = parser.parse_args()
     return args
