436c3ddabf7200bc471296db25e357790a8d0439
Yang Liu <liuyang12@focusmedia.cn>
2019-06-14 14:18:25 +0800

    update

--- demo.py
+++ demo.py
@@ -1,13 +1,6 @@
-import os
-import random
-from shutil import copyfile
-
-import torch
-
 from config import *
 from data_gen import LoadDataset
 from models import Seq2Seq
-from utils import ensure_folder, extract_feature
 
 
 class adict(dict):
@@ -25,14 +18,14 @@ if __name__ == '__main__':
     decoder.eval()
     model = Seq2Seq(encoder, decoder)
 
-    train_loader = LoadDataset('test', text_only=False, data_path=data_path, batch_size=batch_size,
-                               max_timestep=max_timestep, max_label_len=max_label_len, use_gpu=use_gpu, n_jobs=n_jobs,
-                               train_set=train_set, dev_set=dev_set, test_set=test_set, dev_batch_size=dev_batch_size,
-                               decode_beam_size=decode_beam_size)
+    test_loader = LoadDataset('test', text_only=False, data_path=data_path, batch_size=batch_size,
+                              max_timestep=max_timestep, max_label_len=max_label_len, use_gpu=use_gpu, n_jobs=n_jobs,
+                              train_set=train_set, dev_set=dev_set, test_set=test_set, dev_batch_size=dev_batch_size,
+                              decode_beam_size=decode_beam_size)
 
-    print(len(train_loader))
-    print(train_loader[0])
+    print(len(test_loader))
 
-    x, y = train_loader[0]
-    print(x.shape)
-    print(y.shape)
+    for i, (x, y) in enumerate(test_loader):
+        print(x.shape)
+        print(y.shape)
+        break
