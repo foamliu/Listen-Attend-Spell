924b4c111a4362f231b2889f86b3fa67aa954591
Yang Liu <liuyang12@focusmedia.cn>
2019-08-08 15:35:18 +0800

    update

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ def parse_args():
     # decode
     parser.add_argument('--beam_size', default=30, type=int,
                         help='Beam size')
-    parser.add_argument('--nbest', default=1, type=int,
+    parser.add_argument('--nbest', default=5, type=int,
                         help='Nbest size')
     parser.add_argument('--decode_max_len', default=100, type=int,
                         help='Max output length. If ==0 (default), it uses a '
